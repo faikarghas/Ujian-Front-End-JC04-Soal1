93f3bf0656bbcb46feefe06d9cb6f9d920145d1d
Faikar <32967034+faikarghas@users.noreply.github.com>
2018-03-09 10:38:24 +0700

    Update Footer.js

--- src/comp/Footer.js
+++ src/comp/Footer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 class Footer extends Component{
     render(){
         return(
-            <h1>Copyright &copy; irsha valdani 2018</h1>           
+            <h1>Copyright &copy; faikar ghassan 2018</h1>           
         );
     }
 }
